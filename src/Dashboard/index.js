2aea440c84cfc4fe907b77d5c955291068d17376
Troy <36167978+troywsmith@users.noreply.github.com>
2018-10-02 22:07:55 -0400

    implemented simple api usage

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -6,10 +6,31 @@ import "../style.css";
 // import Portfolio from "../Portfolio";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        isLoading: true,
+        data: null,
+    }
+  }
+
+  componentDidMount () {
+      return fetch('https://ws-api.iextrading.com/1.0/stock/aapl/quote')
+          .then ( (response) => response.json() )
+          .then( (responseJson) => {
+              this.setState({
+                  isLoading: false,
+                  data: responseJson,
+              })
+          })
+      .catch((error) => {
+          console.log(error)
+      });
+  }
 
   render() {
-
-    return (
+    if (this.state.isLoading) {
+      return (
       <div className="dashboard">
 
         <div className="section portfolio">
@@ -58,7 +79,48 @@ class Dashboard extends Component {
         </div>
 
       </div>
-    );
+      )
+    } else {
+      return (
+        <div className="dashboard">
+  
+          <div className="section portfolio">
+            <h3>Your Portfolio</h3>
+            <hr />
+            <ul>
+              <li>
+                <div className="left">
+                  <p>{this.state.data.Symbol}</p>
+                </div>
+                <div className="middle">
+                  <p> 48 Shares </p>
+                </div>
+                <div className="right">
+                  <p> @ {this.state.data.latestPrice} </p>
+                </div>
+              </li>
+            </ul> 
+            <hr />
+            <p> Total Balance = $5943.34 </p>
+          </div>
+  
+          <div className="section exchange">
+            <h3>Cash - $5000.00 </h3>
+            <hr />
+            <form action="/newtransaction" method="POST" className="exchangeform">
+              <input id="ticker" type="text" list="datalist" name="ticker" placeholder="  Ticker" />
+              <datalist id="datalist">
+  
+              </datalist>
+              <input id="quantity" type="number" name="quantity" placeholder="  QTY" />
+              <button type="submit">Buy</button>
+              <button type="submit">Sell</button>
+            </form>
+          </div>
+  
+        </div>
+      );
+    }
   }
 }
 
